d1e79282898133aa9cdaa937c53a6f8ceb56815f
Patrick <hellohello@hello.com>
2019-10-22 17:27:07 -0400

    P2: Removed radius-based health depletion

--- Exercises/project 2/js/Predator.js
+++ Exercises/project 2/js/Predator.js
@@ -10,7 +10,7 @@ class Predator {
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  constructor(x, y, speed, fillColor, radius) {
+  constructor(x, y, speed, fillColor, health) {
     // Position
     this.x = x;
     this.y = y;
@@ -19,13 +19,13 @@ class Predator {
     this.vy = 0;
     this.speed = speed;
     // Health properties
-    this.maxHealth = radius;
+    this.maxHealth = health;
     this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
     this.healthLossPerMove = 0.1;
     this.healthGainPerEat = 1;
     // Display properties
     this.fillColor = fillColor;
-    this.radius = this.health; // Radius is defined in terms of health
+    this.radius = 40; // Determines the size of the predator
     // Input properties
     this.upKey = UP_ARROW;
     this.downKey = DOWN_ARROW;
@@ -116,13 +116,12 @@ class Predator {
   // display
   //
   // Draw the predator as an ellipse on the canvas
-  // with a radius the same size as its current health.
+  // with a fixed radius.
   display() {
     push();
     noStroke();
     fill(this.fillColor);
-    this.radius = this.health;
-    ellipse(this.x, this.y, this.radius * 2);
+    ellipse(this.x, this.y, this.radius);
     pop();
     this.displayHealth();
   }
