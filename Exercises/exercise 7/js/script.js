c760e890c9c4d4e1f93079a0166f84aaa35c4eda
Patrick <hellohello@hello.com>
2019-11-10 10:08:23 -0500

    Ex7: Added a title screen placeholder

--- Exercises/exercise 7/js/script.js
+++ Exercises/exercise 7/js/script.js
@@ -1,9 +1,15 @@
-// Predator-Prey Simulation
+// Predator-Prey Simulation Project 3
 // by Pippin Barr
 //
 // Creates a predator and three prey (of different sizes and speeds)
 // The predator chases the prey using the arrow keys and consumes them.
 // The predator loses health over time, so must keep eating to survive.
+//
+// Predator-Prey Simulation source code from Pippin Barr
+//https://github.com/pippinbarr/cart253-2019/blob/master/games/game-oop-predator-prey.zip
+
+// Handling the game's current state/screen
+let state = "TITLE";
 
 // Our predator
 let tiger;
@@ -12,6 +18,17 @@ let tiger;
 let antelope;
 let zebra;
 let bee;
+
+// The art assets
+let titleScreen;
+
+// preload()
+//
+// Preloads the art assets
+function preload() {
+  titleScreen = loadImage("assets/images/TitlePlaceholder.png");
+
+}
 
 // setup()
 //
@@ -29,26 +46,51 @@ function setup() {
 //
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
-  // Clear the background to black
-  background(0);
+  // Changing the game states
+  if (state === "TITLE") {
+    displayTitle();
+  }
 
-  // Handle input for the tiger
-  tiger.handleInput();
+  // Making an if statement to handle the play screen
+  else if (state === "PLAY") {
 
-  // Move all the "animals"
-  tiger.move();
-  antelope.move();
-  zebra.move();
-  bee.move();
+    // Clear the background to black
+    background(0);
 
-  // Handle the tiger eating any of the prey
-  tiger.handleEating(antelope);
-  tiger.handleEating(zebra);
-  tiger.handleEating(bee);
+    // Handle input for the tiger
+    tiger.handleInput();
 
-  // Display all the "animals"
-  tiger.display();
-  antelope.display();
-  zebra.display();
-  bee.display();
+    // Move all the "animals"
+    tiger.move();
+    antelope.move();
+    zebra.move();
+    bee.move();
+
+    // Handle the tiger eating any of the prey
+    tiger.handleEating(antelope);
+    tiger.handleEating(zebra);
+    tiger.handleEating(bee);
+
+    // Display all the "animals"
+    tiger.display();
+    antelope.display();
+    zebra.display();
+    bee.display();
+  }
+}
+
+// displayTitle()
+//
+// Shows the title screen
+function displayTitle() {
+  image(titleScreen, 0, 0, windowWidth, windowHeight);
+}
+
+// mousePressed()
+//
+// Cycles through the various game screens
+function mousePressed() {
+  if (state === "TITLE") {
+    state = "PLAY";
+  }
 }
